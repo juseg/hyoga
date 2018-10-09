40ac61b353402a3ccf8ddd017216dc0629b8b325
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-10-09 15:38:38 +0900

    Add Wikipedia color conventions in new submodule.

---
+++ cartowik/conventions.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2018, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""
+Wikipedia color and linestyle conventions.
+"""
+
+COLORS = dict(
+    # location maps
+    names='#000000',    # toponymes
+    borders='#646464',  # and country labels
+    subject='#FEFEE9',  # primary
+    context='#F6E1B9',  # secondary
+    outside='#E0E0E0',  # off-topic
+    rivers='#0978AB',   # and coasts
+    lakes='#C6ECFF',    # and seas
+    # topographic contours
+    topog19='#F5F4F2',  # light gray
+    topog18='#E0DED8',
+    topog17='#CAC3B8',
+    topog16='#BAAE9A',
+    topog15='#AC9A7C',
+    topog14='#AA8753',  # dark brown
+    topog13='#B9985A',
+    topog12='#C3A76B',
+    topog11='#CAB982',
+    topog10='#D3CA9D',
+    topog09='#DED6A3',
+    topog08='#E8E1B6',
+    topog07='#EFEBC0',  # light yellow
+    topog06='#E1E4B5',
+    topog05='#D1D7AB',
+    topog04='#BDCC96',
+    topog03='#A8C68F',
+    topog02='#94BF8B',  # dark green
+    topog01='#ACD0A5',
+    topog00='#A7DFD2',  # depressions
+    # bathymetry contours
+    bathy00='#D8F2FE',  # light blue
+    bathy01='#C6ECFF',
+    bathy02='#B9E3FF',
+    bathy03='#ACDBFB',
+    bathy04='#A1D2F7',
+    bathy05='#96C9F0',
+    bathy06='#8DC1EA',
+    bathy07='#84B9E3',
+    bathy08='#79B2DE',
+    bathy09='#71ABD8',  # dark blue
+)
