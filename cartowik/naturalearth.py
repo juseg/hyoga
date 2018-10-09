a4a7c49ba1b8609515b7befabded497deecc9a39
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-10-09 13:34:11 +0900

    Make private NE wrapper work for all features.

--- cartowik/naturalearth.py
+++ cartowik/naturalearth.py
@@ -8,15 +8,17 @@ Natural Earth plotting tools.
 import matplotlib.pyplot as plt
 import shapely.geometry as sgeom
 import cartopy.crs as ccrs
-import cartopy.feature as cfeature
 import cartopy.io.shapereader as cshp
 
 
 # Natural Earth internals
 # -----------------------
 
-def _add_topical_layer(ax=None, category=None, name=None, scale=None,
-                       facecolor=None, subject=None, subject_facecolor=None):
+def _add_subject_feature(ax=None, category=None, name=None, scale=None,
+                         facecolor=None, subject=None, subject_facecolor=None,
+                         **kwargs):
+    """A re-implementation of Cartopy's GeoAxes.add_feature allowing different
+    edge and face colors for the subject's geometry."""
 
     # get current axes if None provided
     ax = ax or plt.gca()
@@ -34,12 +36,14 @@ def _add_topical_layer(ax=None, category=None, name=None, scale=None,
     for rec in shp.records():
         attr = 'name'
         attr = attr if attr in rec.attributes else attr.upper()
-        name = rec.attributes[attr]
+        if attr in rec.attributes and rec.attributes[attr] == subject:
+            color = subject_facecolor
+        else:
+            color = facecolor
 
         # add intersecting geometries
         if rec.geometry is not None and axes_box.intersects(rec.geometry):
-            color = (subject_facecolor if name == subject else facecolor)
-            ax.add_geometries(rec.geometry, crs, facecolor=color)
+            ax.add_geometries(rec.geometry, crs, facecolor=color, **kwargs)
 
 
 # Natural Earth cultural
@@ -47,75 +51,69 @@ def _add_topical_layer(ax=None, category=None, name=None, scale=None,
 
 def add_countries(ax=None, facecolor='#e0e0e0', scale='10m',
                   subject=None, subject_facecolor='#fefee9'):
-    _add_topical_layer(
+    _add_subject_feature(
         ax=ax, category='cultural', name='admin_0_countries', scale=scale,
         subject=subject, subject_facecolor=subject_facecolor,
         facecolor=facecolor)
 
 
 def add_country_borders(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='cultural', name='admin_0_boundary_lines_land', scale=scale,
-        edgecolor='#646464', facecolor='none', linewidth=2.0))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='cultural', name='admin_0_boundary_lines_map_units',
-        scale=scale, edgecolor='#646464', facecolor='none', linewidth=1.5))
+    _add_subject_feature(
+        ax=ax, category='cultural', name='admin_0_boundary_lines_land',
+        scale=scale, edgecolor='#646464', facecolor='none', linewidth=2.0)
+    _add_subject_feature(
+        ax=ax, category='cultural', name='admin_0_boundary_lines_map_units',
+        scale=scale, edgecolor='#646464', facecolor='none', linewidth=1.5)
 
 
 def add_states(ax=None, facecolor='#e0e0e0', scale='10m',
                subject=None, subject_facecolor='#fefee9'):
-    _add_topical_layer(
+    _add_subject_feature(
         ax=ax, category='cultural', name='admin_1_states_provinces',
         scale=scale, subject=subject, subject_facecolor=subject_facecolor,
         facecolor=facecolor)
 
 
 def add_state_borders(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='cultural', name='admin_1_states_provinces_lines',
-        scale=scale, edgecolor='#646464', facecolor='none', linewidth=1.0))
+    _add_subject_feature(
+        ax=ax, category='cultural', name='admin_1_states_provinces_lines',
+        scale=scale, edgecolor='#646464', facecolor='none', linewidth=1.0)
 
 
 # Natural Earth physical
 # ----------------------
 
 def add_coastline(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='coastline', scale=scale,
-        edgecolor='#0978ab', facecolor='none', lw=0.25))
+    _add_subject_feature(
+        ax=ax, category='physical', name='coastline', scale=scale,
+        edgecolor='#0978ab', facecolor='none', lw=0.25)
 
 
 def add_lakes(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='lakes', scale=scale,
-        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='lakes_europe', scale=scale,
-        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='lakes_north_america', scale=scale,
-        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25))
+    _add_subject_feature(
+        ax=ax, category='physical', name='lakes', scale=scale,
+        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25)
+    _add_subject_feature(
+        ax=ax, category='physical', name='lakes_europe', scale=scale,
+        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25)
+    _add_subject_feature(
+        ax=ax, category='physical', name='lakes_north_america', scale=scale,
+        edgecolor='#0978ab', facecolor='#d8f2fe', lw=0.25)
 
 
 def add_ocean(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='ocean', scale=scale,
-        edgecolor='#0978ab', facecolor='#c6ecff', lw=0.25))
+    _add_subject_feature(
+        ax=ax, category='physical', name='ocean', scale=scale,
+        edgecolor='#0978ab', facecolor='#c6ecff', lw=0.25)
 
 
 def add_rivers(ax=None, scale='10m'):
-    ax = ax or plt.gca()
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='rivers_lake_centerlines', scale=scale,
-        edgecolor='#0978ab', facecolor='none', lw=0.5))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='rivers_europe', scale=scale,
-        edgecolor='#0978ab', facecolor='none', lw=0.5))
-    ax.add_feature(cfeature.NaturalEarthFeature(
-        category='physical', name='rivers_north_america', scale=scale,
-        edgecolor='#0978ab', facecolor='none', lw=0.5))
+    _add_subject_feature(
+        ax=ax, category='physical', name='rivers_lake_centerlines',
+        scale=scale, edgecolor='#0978ab', facecolor='none', lw=0.5)
+    _add_subject_feature(
+        ax=ax, category='physical', name='rivers_europe', scale=scale,
+        edgecolor='#0978ab', facecolor='none', lw=0.5)
+    _add_subject_feature(
+        ax=ax, category='physical', name='rivers_north_america', scale=scale,
+        edgecolor='#0978ab', facecolor='none', lw=0.5)
