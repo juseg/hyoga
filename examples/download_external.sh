d84e5ec98ad8db20d9c1eed07e47da8541eb2e23
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2018-10-25 10:55:00 +0900

    Add Hokkaido SRTM topographic data download.

--- examples/download_external.sh
+++ examples/download_external.sh
@@ -11,3 +11,20 @@ cd external
 # download CleanTOPO (full version) worldwide elevation data
 wget -nc http://naciscdn.org/data/cleantopo/Full.zip
 unzip -jn Full.zip Full/CleanTOPO2.{tif,tfw}
+
+# download Hokkaido SRTM land topographic data
+if [ ! -f srtm_hokkaido.tif ]
+then
+
+    # download SRTM data
+    serv="http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff"
+    for tile in srtm_{64_04,65_03,65_04,66_04}
+    do
+        wget -nc $serv/$tile.zip
+        unzip -n $tile.zip $tile.{hdr,tfw,tif}
+    done
+
+    # build virtual patch
+    gdalbuildvrt srtm.vrt srtm_??_??.tif
+
+fi
