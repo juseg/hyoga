b90bad215721a543ae7257e51e2d3fa1b42f7018
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-12-04 18:33:13 +0100

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,12 @@
+.. Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+.. GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
+
+PISMX
+=====
+
+A place where I hope to gather various tools to run paleoglacier simulations
+with PISM_, some day. Currently a very thin wrapper around xarray_ to read
+PISM_ output files with age coordinates.
+
+.. _PISM: http://pism-docs.org
+.. _xarray: http://xarray.pydata.org
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""Build script for pismx."""
+
+import setuptools
+
+with open('README.rst', 'r') as f:
+    README = f.read()
+
+setuptools.setup(
+    name='pismx',
+    version='0.0',
+    author='Julien Seguinot',
+    author_email='seguinot@vaw.baug.ethz.ch',
+    description='A thin wrapper around xarray to open PISM files',
+    long_description=README,
+    long_description_content_type='text/x-rst',
+    url='http://github.com/juseg/pismx',
+    license='gpl-3.0',
+    install_requires=['xarray'],
+    py_modules=['pismx'],
+)
