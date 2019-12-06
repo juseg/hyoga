c0b9af06ff59be88dbce93f177e05669b250ff5f
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2019-12-06 10:39:24 +0100

    Add dataset open methods.

---
+++ pismx/__init__.py
@@ -0,0 +1,2 @@
+# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
---
+++ pismx/open.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""
+A thin wrapper around xarray to open PISM output files with age coordinates.
+"""
+
+import xarray as xr
+
+
+def _assign_age_dim(ds):
+    """Assign age dimension to a dataset with time coordinates."""
+    if 'time' in ds.coords and 'seconds' in ds.time.units:
+        ds = ds.assign_coords(age=(-ds.time/(1e3*365*24*60*60)).assign_attrs(
+            long_name='model age', units='ka')).swap_dims(dict(time='age'))
+    elif 'time' in ds.coords and 'years' in ds.time.units:
+        ds = ds.assign_coords(age=(-ds.time/1e3).assign_attrs(
+            long_name='model age', units='ka')).swap_dims(dict(time='age'))
+    return ds
+
+
+def dataset(filename):
+    """Open single-file dataset with age coordinate."""
+    ds = xr.open_dataset(filename, decode_cf=False)
+    ds = _assign_age_dim(ds)
+    return ds
+
+
+def mfdataset(filename):
+    """Open multi-file dataset with age coordinate."""
+    ds = xr.open_mfdataset(filename, combine='by_coords', decode_cf=False)
+    ds = _assign_age_dim(ds)
+    return ds
