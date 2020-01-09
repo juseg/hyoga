0dc1c9211372193bf42b03f099ef930c7a19a4dc
Julien Seguinot <seguinot@vaw.baug.ethz.ch>
2020-01-10 00:17:56 +0100

    Use last file's attrs, allow kwargs in open funcs.

--- pismx/open.py
+++ pismx/open.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
+# Copyright (c) 2019--2020, Julien Seguinot <seguinot@vaw.baug.ethz.ch>
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
@@ -6,6 +6,7 @@ A thin wrapper around xarray to open PISM output files with age coordinates.
 """
 
 import os
+import glob
 import xarray as xr
 
 
@@ -20,16 +21,20 @@ def _assign_age_dim(ds):
     return ds
 
 
-def dataset(filename):
+def dataset(filename, **kwargs):
     """Open single-file dataset with age coordinate."""
-    ds = xr.open_dataset(filename, decode_cf=False)
+    ds = xr.open_dataset(filename, decode_cf=False, **kwargs)
     ds = _assign_age_dim(ds)
     return ds
 
 
-def mfdataset(filename):
+def mfdataset(filename, **kwargs):
     """Open multi-file dataset with age coordinate."""
     filename = os.path.expanduser(filename)
-    ds = xr.open_mfdataset(filename, combine='by_coords', decode_cf=False)
+    # get global attributes from last file (issue #2382 fixed in juseg/xarray)
+    # do not concatenate lon, lat etc (data_vars='minimal')
+    ds = xr.open_mfdataset(
+        filename, attrs_file=sorted(glob.glob(filename))[-1],
+        combine='by_coords', data_vars='minimal', decode_cf=False, **kwargs)
     ds = _assign_age_dim(ds)
     return ds
