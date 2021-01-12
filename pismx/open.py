d88398ec46a9a5e2afbb89a4e172845c0c0c7517
Julien Seguinot <juseg@posteo.eu>
2021-01-12 09:43:24 +0100

    Allow visual data interp along axis pixel coords.

--- pismx/open.py
+++ pismx/open.py
@@ -13,6 +13,9 @@ import xarray as xr
 from scipy import ndimage
 
 
+# Private methods
+# ---------------
+
 def _assign_age_dim(ds):
     """Assign age dimension to a dataset with time coordinates."""
     if 'time' in ds.coords and 'seconds' in ds.time.units:
@@ -23,6 +26,33 @@ def _assign_age_dim(ds):
             long_name='model age', units='ka')).swap_dims(dict(time='age'))
     return ds
 
+
+def _coords_from_axes(ax):
+    """Compute coordinate vectors from matplotlib axes."""
+    bbox = ax.get_window_extent()
+    return _coords_from_extent(ax.get_extent(), bbox.width, bbox.height)
+
+
+def _coords_from_extent(extent, cols, rows):
+    """Compute coordinate vectors from image extent."""
+
+    # compute dx and dy
+    (west, east, south, north) = extent
+    dx = (east-west) / cols
+    dy = (north-south) / rows
+
+    # prepare coordinate vectors
+    xwcol = west + 0.5*dx  # x-coord of W row cell centers
+    ysrow = south + 0.5*dy  # y-coord of N row cell centers
+    x = xwcol + np.arange(cols)*dx  # from W to E
+    y = ysrow + np.arange(rows)*dy  # from S to N
+
+    # return coordinate vectors
+    return x, y
+
+
+# Public methods
+# --------------
 
 def dataset(filename, **kwargs):
     """Open single-file dataset with age coordinate."""
@@ -60,19 +90,28 @@ def subdataset(filename, time, shift=0, **kwargs):
     return ds
 
 
-def visual(filename, bootfile, interpfile, time, sigma=None, **kwargs):
+def visual(filename, bootfile, interpfile, time, ax=None, sigma=None,
+           **kwargs):
     """Open interpolated output for visualization."""
     # IDEA: would it make more sense as a dataset method?
-    # pismx.open.dataset(...).visual(...)
-    # pismx.open.subdataset(...).visual(...)
+    # the way to go about this apparently is through accessors
+    # http://xarray.pydata.org/en/stable/internals.html#extending-xarray
+    # pismx.open.dataset(...).px.visual(...)
+    # pismx.open.subdataset(...).px.visual(...)
 
     # load hires bedrock topography
     with dataset(interpfile) as ds:
         hires = ds.usurf.fillna(0.0) - ds.thk.fillna(0.0)
-        x = hires.x  # FIXME coords from axes
-        y = hires.y  # FIXME coords from axes
+
+    # interpolation coordinates
+    if ax is not None:
+        x, y = _coords_from_axes(ax)
+    else:
+        x = hires.x
+        y = hires.y
 
     # try to smooth integer-precision steps
+    # IDEA: can we avoid scipy.ndimage?
     if sigma is not None:
         dx = (x[-1]-x[0])/(len(x)-1)
         dy = (y[-1]-y[0])/(len(y)-1)
