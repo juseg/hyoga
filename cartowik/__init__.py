93c7746ac63d14378d0fa3ff3bae39a2b0d6dfcd
Julien Seguinot <juseg@posteo.eu>
2022-08-05 14:32:29 +0200

    Move cartowik modules to their new locations.

--- cartowik/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2018, Julien Seguinot (juseg.github.io)
-# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
-
-"""
-Cartowik: draw Wikipedia style location and topographic maps.
-"""
--- hyoga/calc/__init__.py
+++ hyoga/calc/__init__.py
@@ -2,5 +2,5 @@
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
-Hyoga plotting tools to visualize glacier modelling datasets.
+Hyoga calculus tools to manipulate glacier modelling datasets.
 """
