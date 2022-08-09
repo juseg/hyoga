ab8d25c22365cdca4ca6fba9776eeee0f26d94f5
Julien Seguinot <juseg@posteo.eu>
2022-08-09 12:25:19 +0200

    Hide color, level data, break *_contours (#15).

--- hyoga/plot/colormaps.py
+++ hyoga/plot/colormaps.py
@@ -1,119 +1,59 @@
-# Copyright (c) 2018, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2018-2022, Julien Seguinot (juseg.github.io)
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
-Wikipedia color and linestyle conventions.
+This module defines new altitude and relief-shading colormaps for matplotlib.
+The color definitions are based on Wikipedia's "WikiProject Maps" cartographic
+[conventions](https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Maps/Conventions)
+for topographic maps.
 """
 
 import numpy as np
 import matplotlib.colors as mcolors
 
+# bathymetric levels optimized for [6000, 0] and example rescaling
+_bathymetric_colors = [(1+level/6000, color) for (level, color) in [
+    (-6000, '#71ABD8'),   # 9000 6000 3000 dark blue
+    (-3000, '#79B2DE'),   # 4500 3000 1500
+    (-2000, '#84B9E3'),   # 3000 2000 1000
+    (-1500, '#8DC1EA'),   # 2250 1500  750
+    (-1000, '#96C9F0'),   # 1500 1000  500
+    (-750,  '#A1D2F7'),   # 1125  750  325
+    (-500,  '#ACDBFB'),   # :750  500  250
+    (-250,  '#B9E3FF'),   # :375  250  125
+    (-100,  '#C6ECFF'),   # :150  100   50
+    (-0,    '#D8F2FE')]]  # :  0    0    0 light blue
 
-# Wikipedia color conventions
-# ---------------------------
-
-COLORS = dict(
-    # location maps
-    names='#000000',    # toponymes
-    borders='#646464',  # and country labels
-    subject='#FEFEE9',  # primary
-    context='#F6E1B9',  # secondary
-    outside='#E0E0E0',  # off-topic
-    rivers='#0978AB',   # and coasts
-    lakes='#C6ECFF',    # and seas
-    # topographic contours
-    topog19='#F5F4F2',  # light gray
-    topog18='#E0DED8',
-    topog17='#CAC3B8',
-    topog16='#BAAE9A',
-    topog15='#AC9A7C',
-    topog14='#AA8753',  # dark brown
-    topog13='#B9985A',
-    topog12='#C3A76B',
-    topog11='#CAB982',
-    topog10='#D3CA9D',
-    topog09='#DED6A3',
-    topog08='#E8E1B6',
-    topog07='#EFEBC0',  # light yellow
-    topog06='#E1E4B5',
-    topog05='#D1D7AB',
-    topog04='#BDCC96',
-    topog03='#A8C68F',
-    topog02='#94BF8B',  # dark green
-    topog01='#ACD0A5',
-    topog00='#A7DFD2',  # depressions
-    # bathymetry contours
-    bathy00='#D8F2FE',  # light blue
-    bathy01='#C6ECFF',
-    bathy02='#B9E3FF',
-    bathy03='#ACDBFB',
-    bathy04='#A1D2F7',
-    bathy05='#96C9F0',
-    bathy06='#8DC1EA',
-    bathy07='#84B9E3',
-    bathy08='#79B2DE',
-    bathy09='#71ABD8',  # dark blue
-)
-
-CLISTS = dict(
-    Bathymetric=[COLORS['bathy{:02d}'.format(9-i)] for i in range(10)],
-    Topographic=[COLORS['topog{:02d}'.format(i)] for i in range(20)],
-)
-
-
-# Contour level conventions
-# -------------------------
-
-LEVELS = dict(
-    Bathymetric=[  # -color-  -example linear rescaling-
-        -6000,     # bathy09   9000 6000 3000 dark blue
-        -3000,     # bathy08   4500 3000 1500
-        -2000,     # bathy07   3000 2000 1000
-        -1500,     # bathy06   2250 1500  750
-        -1000,     # bathy05   1500 1000  500
-        -750,      # bathy04   1125  750  325
-        -500,      # bathy03    750  500  250
-        -250,      # bathy02    375  250  125
-        -100,      # bathy01    150  100   50
-        -0,        # bathy00      0    0    0 light blue
-    ],
-    Topographic=[  # -color-  -example linear rescaling-
-        0,         # topog01      0     0    0    0    0
-        50,        # topog02    100    75   50   33   25 dark green
-        100,       # topog03    200   150  100   67   50
-        250,       # topog04    500   375  250  167  125
-        500,       # topog05   1000   750  500  333  250
-        750,       # topog06   1500  1125  750  500  375
-        1000,      # topog07   2000  1500 1000  667  500 light yellow
-        1500,      # topog08   3000  2250 1500 1000  750
-        2000,      # topog09   4000  3000 2000 1333 1000
-        2500,      # topog10   5000  3750 2500 1667 1250
-        3000,      # topog11   6000  4500 3000 2000 1500
-        3500,      # topog12   7000  5250 3500 2333 1750
-        4000,      # topog13   8000  6000 4000 2667 2000
-        4500,      # topog14   9000  6750 4500 3000 2550 dark brown
-        5000,      # topog15  10000  7500 5000 3333 2500
-        6000,      # topog16  12000  9000 6000 4000 3000
-        7000,      # topog17  14000 10500 7000 4667 3500
-        8000,      # topog18  16000 12000 8000 5333 4000
-        9000,      # topog19  18000 13500 9000 6000 4500 light grey
-    ],
-)
-
-
-# Cartowik colormaps
-# ------------------
+# topographic levels optimized for [0, 9000] and example rescaling
+_topographic_colors = [(level/9000, color) for (level, color) in [
+    (0,     '#ACD0A5'),   # :   0     0    0    0    0
+    (50,    '#94BF8B'),   # : 100    75   50   33   25 dark green
+    (100,   '#A8C68F'),   # : 200   150  100   67   50
+    (250,   '#BDCC96'),   # : 500   375  250  167  125
+    (500,   '#D1D7AB'),   # :1000   750  500  333  250
+    (750,   '#E1E4B5'),   # :1500  1125  750  500  375
+    (1000,  '#EFEBC0'),   # :2000  1500 1000  667  500 light yellow
+    (1500,  '#E8E1B6'),   # :3000  2250 1500 1000  750
+    (2000,  '#DED6A3'),   # :4000  3000 2000 1333 1000
+    (2500,  '#D3CA9D'),   # :5000  3750 2500 1667 1250
+    (3000,  '#CAB982'),   # :6000  4500 3000 2000 1500
+    (3500,  '#C3A76B'),   # :7000  5250 3500 2333 1750
+    (4000,  '#B9985A'),   # :8000  6000 4000 2667 2000
+    (4500,  '#AA8753'),   # :9000  6750 4500 3000 2550 dark brown
+    (5000,  '#AC9A7C'),   # 10000  7500 5000 3333 2500
+    (6000,  '#BAAE9A'),   # 12000  9000 6000 4000 3000
+    (7000,  '#CAC3B8'),   # 14000 10500 7000 4667 3500
+    (8000,  '#E0DED8'),   # 16000 12000 8000 5333 4000
+    (9000,  '#F5F4F2')]]  # 18000 13500 9000 6000 4500 light grey
 
 # topographic colormaps
 
 BATHYMETRIC = mcolors.LinearSegmentedColormap.from_list(
-    'Bathymetric', list(zip([1+l/6000 for l in LEVELS['Bathymetric']],
-                            CLISTS['Bathymetric'])), N=4096)
+    'Bathymetric', _bathymetric_colors, N=4096)
 
 TOPOGRAPHIC = mcolors.LinearSegmentedColormap.from_list(
-    'Topographic', list(zip([l/9000 for l in LEVELS['Topographic']],
-                            CLISTS['Topographic'][1:])), N=4096)
-TOPOGRAPHIC.set_under(COLORS['topog00'])
+    'Topographic', _topographic_colors, N=4096)
+TOPOGRAPHIC.set_under('#A7DFD2')
 
 ELEVATIONAL = mcolors.LinearSegmentedColormap.from_list('Elevational', [
     *BATHYMETRIC(np.linspace(0, 1, 2048)),
