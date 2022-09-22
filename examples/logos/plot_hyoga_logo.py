dc50d9919247ff9044a1cbf0ce6409da1c561cd9
Julien Seguinot <juseg@posteo.eu>
2022-09-22 16:01:18 +0200

    Last changes in logo, monochrome white (#20).

--- examples/logos/plot_hyoga_logo.py
+++ examples/logos/plot_hyoga_logo.py
@@ -11,11 +11,12 @@ import hyoga.plot
 
 # initialize figure
 fig = plt.figure(figsize=(9.6, 3.2))
-fig.patch.set_alpha(0)
-ax = fig.add_axes([17/48, 0, 14/48, 7/8], projection=cartopy.crs.Orthographic(
+fig.patch.set_facecolor('0.25')  # ignored by transparent=True
+ax = fig.add_axes(
+    [17/48, 1/32, 14/48, 13/16], projection=cartopy.crs.Orthographic(
         central_longitude=-45, central_latitude=90))
-ax.patch.set_facecolor('tab:blue')
-ax.spines['geo'].set_edgecolor('none')
+ax.patch.set_facecolor('none')
+ax.spines['geo'].set(capstyle='round', edgecolor='w', linewidth=16)
 
 # ax.set_extent does not work well with ortho proj
 ax.set_xlim((-6.4e6, 6.4e6))
@@ -27,12 +28,12 @@ hyoga.plot.paleoglaciers(ax=ax, alpha=0.75, facecolor='w', source='bat19')
 hyoga.plot.glaciers(ax=ax, edgecolor='w', facecolor='w', scale='50m')
 
 # add text and overline
-fontsize = 7/8 * fig.get_window_extent().height / fig.dpi * 72
-kwargs = dict(color='tab:blue', fontsize=fontsize, va='bottom')
-fig.text(1/3, 0, 'hy', ha='right', **kwargs)
-fig.text(2/3, 0, 'ga', ha='left', **kwargs)
-ax.fill_between([5/12, 7/12], 15/16, 1, clip_on=False, facecolor='tab:blue',
-                transform=fig.transFigure)
+fig.text(
+    1/2, 0, 'hy  ga', color='w', family='monospace', ha='center', va='bottom',
+    fontsize=7/8*fig.get_window_extent().height/fig.dpi*72)
+ax.fill_between(
+    [5/12, 7/12], 15/16, 1, clip_on=False, facecolor='w',
+    transform=fig.transFigure)
 
 # save static file
-# fig.savefig('../../doc/_static/hyoga_logo.png')
+# fig.savefig('../../doc/_static/png/hyoga_logo.png', transparent=True)
