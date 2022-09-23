e862d365c6daae2ae6ff677e2fbd8b74e7b7197a
Julien Seguinot <juseg@posteo.eu>
2022-09-23 07:53:52 +0200

    Add logo dosctring title needed for gallery (#20).

--- examples/logos/plot_hyoga_logo.py
+++ examples/logos/plot_hyoga_logo.py
@@ -2,7 +2,12 @@
 # Copyright (c) 2021-2022, Julien Seguinot (juseg.github.io)
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
-"""Plot hyoga logo."""
+"""
+Plot hyoga logo
+===============
+
+Plot default white monochrome logo including glaciers and paleoglaciers.
+"""
 
 import matplotlib.pyplot as plt
 import cartopy
