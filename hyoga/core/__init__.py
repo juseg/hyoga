df13283d8d209c0c1a99d341e92dda2255e98d5b
Julien Seguinot <juseg@posteo.eu>
2022-09-26 15:31:01 +0200

    Move cartowik profile tools to (tmp) core.profile.

--- hyoga/core/__init__.py
+++ hyoga/core/__init__.py
@@ -2,5 +2,5 @@
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
-Hyoga calculus tools to manipulate glacier modelling datasets.
+Hyoga core classes to manipulate glacier modelling datasets.
 """
