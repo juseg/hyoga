9322b4ce29a55625a7d8a557cc04e710df9ac693
Julien Seguinot <juseg@posteo.eu>
2022-10-05 16:19:39 +0200

    Add hillshade colormaps example (#19).

---
+++ examples/cartography/plot_hillshade_colormaps.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# Copyright (c) 2022, Julien Seguinot (juseg.github.io)
+# GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""
+Hillshade colormaps
+===================
+
+Plot shaded relief map using glossy and matte colormaps.
+"""
+
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import hyoga.open
+
+# initialize figure
+fig, axes = plt.subplots(ncols=2, subplot_kw=dict(projection=ccrs.UTM(32)))
+
+# open demo data with refined topography
+with hyoga.open.example('pism.alps.vis.refined.nc') as ds:
+    ds = ds.sel(x=slice(380e3, 435e3))
+
+    # plot bedrock altitude
+    for ax in axes:
+        ds.hyoga.plot.bedrock_altitude(ax=ax, cmap='Topographic', vmin=0)
+
+    # add hillshades
+    ds.hyoga.plot.bedrock_hillshade(ax=axes[0], cmap='Glossy')
+    ds.hyoga.plot.bedrock_hillshade(ax=axes[1], cmap='Matte', vmin=0)
+
+    # set titles
+    axes[0].set_title('Glossy')
+    axes[1].set_title('Matte')
+
+# show
+plt.show()
