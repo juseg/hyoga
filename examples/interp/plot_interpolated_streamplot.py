d6c8a1d41d8e169fa1b59ca83b7c508070b1a10b
Julien Seguinot <juseg@posteo.eu>
2022-10-24 09:20:42 +0200

    Fix extent, simplify interpolated streamplot.
    I'm confused: the streamplot extent in interpolated streamplot example
    seem to have been altered by 3c561c930afde0113b7dce9cd42e724aaabca515 and
    the move from Cartopy GeoAxes to normal axes. Actually streamplot on
    original data would require to crop the data to the same extent as
    interpolated data, otherwise density would be much lower. I can't
    understand how this worked in v0.1.2 without any cropping.
    
    Anyway I did some testing and results from interpolated and original
    data are visually indiscernible. Very unsure what happened on GeoAxes.

--- examples/interp/plot_interpolated_streamplot.py
+++ examples/interp/plot_interpolated_streamplot.py
@@ -8,8 +8,8 @@ Interpolated streamplot
 
 Demonstrate combining two-dimensional model output spatial interpolation with a
 surface velocity streamplot. The streamplot method does an interpolation on its
-own, and consumes a lot of memory when applied on large arrays. Therefore it is
-advisable to run it on the original rather than the interpolated data.
+own. When applied to larger domains it may be advisable to run it on the
+original rather than interpolated data.
 """
 
 import matplotlib.pyplot as plt
@@ -22,15 +22,15 @@ cax = plt.axes([0.15, 0.55, 0.025, 0.25])
 # open demo data
 with hyoga.open.example('pism.alps.out.2d.nc') as ds:
     ds = ds.hyoga.assign_isostasy(hyoga.open.example('pism.alps.in.boot.nc'))
-    interp = ds.hyoga.interp(hyoga.open.example('pism.alps.vis.refined.nc'))
+    ds = ds.hyoga.interp(hyoga.open.example('pism.alps.vis.refined.nc'))
 
     # plot model output
-    interp.hyoga.plot.bedrock_altitude(
+    ds.hyoga.plot.bedrock_altitude(
         ax=ax, cmap='gist_earth', vmin=-3000, vmax=4500)
-    interp.hyoga.plot.surface_altitude_contours(ax=ax)
-    interp.hyoga.plot.ice_margin(ax=ax, facecolor='w')
+    ds.hyoga.plot.surface_altitude_contours(ax=ax)
+    ds.hyoga.plot.ice_margin(ax=ax, facecolor='w')
 
-    # plot streamplot from non-interpolated data
+    # plot streamplot
     streams = ds.hyoga.plot.surface_velocity_streamplot(
         ax=ax, cmap='Blues', vmin=1e1, vmax=1e3, density=(6, 4))
 
@@ -41,6 +41,7 @@ with hyoga.open.example('pism.alps.out.2d.nc') as ds:
 ax.set_title(r'Interpolated output (m$\,$a$^{-1}$)')
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
+ax.set_aspect('equal')
 
 # show
 plt.show()
