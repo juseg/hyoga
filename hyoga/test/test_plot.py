213934669df98ef0a305a951a517b587a4bc1828
Julien Seguinot <juseg@posteo.eu>
2022-12-16 10:48:28 +0100

    Add missing coordinates in test dataset.

--- hyoga/test/test_plot.py
+++ hyoga/test/test_plot.py
@@ -1,6 +1,10 @@
 # Copyright (c) 2022, Julien Seguinot (juseg.github.io)
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
+"""
+This module contains basic tests for accessor plot methods.
+"""
+
 import numpy as np
 import xarray as xr
 import hyoga
@@ -8,10 +12,18 @@ import hyoga
 
 def make_dataset():
     """Make minimal dataset with bedrock altitude and ice thickness."""
-    darray = xr.DataArray(np.linspace(0, 1000, 6).reshape((2, 3)))
-    ds = xr.Dataset({
-        'b': darray.assign_attrs(standard_name='bedrock_altitude'),
-        'h': darray.assign_attrs(standard_name='land_ice_thickness')})
+    x = np.arange(2)
+    y = np.arange(3)
+    z = np.linspace(0, 1000, 6).reshape((2, 3))
+    ds = xr.Dataset(
+        coords={
+            'x': (['x'], x),
+            'y': (['y'], y),
+            },
+        data_vars={
+            'b': (['x', 'y'], z, {'standard_name': 'bedrock_altitude'}),
+            'h': (['x', 'y'], z, {'standard_name': 'land_ice_thickness'}),
+            })
     return ds
 
 
