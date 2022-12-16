f0a019d7a3d941497a3e540c602655349ad7d145
Julien Seguinot <juseg@posteo.eu>
2022-12-16 10:12:04 +0100

    Add basic tests for all plot methods.

--- hyoga/test/test_plot.py
+++ hyoga/test/test_plot.py
@@ -15,6 +15,71 @@ def make_dataset():
     return ds
 
 
+def test_bedrock_altitude():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_altitude()
+
+
+def test_bedrock_altitude_contours():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_altitude_contours()
+
+
+def test_bedrock_erosion():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_erosion()
+
+
+def test_bedrock_hillshade():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_hillshade()
+
+
+def test_bedrock_isostasy():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_isostasy()
+
+
+def test_bedrock_shoreline():
+    ds = make_dataset()
+    ds.hyoga.plot.bedrock_shoreline()
+
+
 def test_ice_margin():
     ds = make_dataset()
     ds.hyoga.plot.ice_margin()
+
+
+def test_surface_altitude_contours():
+    ds = make_dataset()
+    ds.hyoga.plot.surface_altitude_contours()
+
+
+def test_surface_hillshade():
+    ds = make_dataset()
+    ds.hyoga.plot.surface_hillshade()
+
+
+def test_surface_velocity():
+    ds = make_dataset()
+    ds.hyoga.plot.surface_velocity()
+
+
+def test_surface_velocity_streamplot():
+    ds = make_dataset()
+    ds.hyoga.plot.surface_velocity_streamplot()
+
+
+def test_natural_earth():
+    ds = make_dataset()
+    ds.hyoga.plot.naturalearth()
+
+
+def test_paleoglaciers():
+    ds = make_dataset()
+    ds.hyoga.plot.paleoglaciers()
+
+
+def test_scale_bar():
+    ds = make_dataset()
+    ds.hyoga.plot.scale_bar()
