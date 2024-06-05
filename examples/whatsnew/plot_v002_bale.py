f2dec7b0977b213eec061975450e395afb3ab99b
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-06-05 18:08:23 +0200

    Fix documentation warnings, outdated credits. (#80)
    * Remove dated (unneeded) theme options.
    
    * Fix broken link, very unrealistic timeline. :-)
    
    * Replace Stamen with Esri in release plots.
    
    See http://maps.stamen.com/stadia-partnership/.
    
    * Update copyright year in footer.
    
    * Remove outdated Bergen credits.

--- examples/whatsnew/plot_v002_bale.py
+++ examples/whatsnew/plot_v002_bale.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2022-2023, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2022-2024, Julien Seguinot (juseg.dev)
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
@@ -13,14 +13,14 @@ import hyoga
 
 # plot paleoglaciers
 ax = hyoga.open.paleoglaciers().to_crs(epsg=3857).plot(
-    alpha=0.75, facecolor='w', edgecolor='w', linewidth=2)
+    alpha=0.75, facecolor='tab:blue', edgecolor='tab:blue', linewidth=2)
 
 # zoom on Bale Mountains (80x60 km)
 ax.set_xlim(4390e3, 4470e3)
 ax.set_ylim(730e3, 790e3)
 
 # add stamen terrain
-cx.add_basemap(ax, source=cx.providers.Stamen.TerrainBackground)
+cx.add_basemap(ax, source=cx.providers.Esri.WorldShadedRelief)
 
 # set axes properties
 ax.set_title('Bale Mountains')
--- examples/whatsnew/plot_v003_cocuy.py
+++ examples/whatsnew/plot_v003_cocuy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2023, Julien Seguinot (juseg.github.io)
+# Copyright (c) 2023-2024, Julien Seguinot (juseg.github.io)
 # GNU General Public License v3.0+ (https://www.gnu.org/licenses/gpl-3.0.txt)
 
 """
@@ -13,14 +13,14 @@ import hyoga
 
 # plot paleoglaciers
 ax = hyoga.open.paleoglaciers().to_crs(epsg=3857).plot(
-    alpha=0.75, facecolor='w', edgecolor='w', linewidth=2)
+    alpha=0.75, facecolor='tab:blue', edgecolor='tab:blue', linewidth=2)
 
 # zoom on Cocuy (240x180 km)
 ax.set_xlim(-8200e3, -7960e3)
 ax.set_ylim(580e3, 760e3)
 
 # add stamen terrain
-cx.add_basemap(ax, source=cx.providers.Stamen.TerrainBackground)
+cx.add_basemap(ax, source=cx.providers.Esri.WorldShadedRelief)
 
 # set axes properties
 ax.set_title('Cocuy')
