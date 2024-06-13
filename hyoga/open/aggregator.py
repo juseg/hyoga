eaa6b52b0859c8f5a75b5336d81feed2d6f0ab5e
Julien Seguinot <1186928+juseg@users.noreply.github.com>
2024-06-13 15:40:19 +0200

    Add cw5e5 climate aggregator (wip).

--- hyoga/open/aggregator.py
+++ hyoga/open/aggregator.py
@@ -87,3 +87,41 @@ class Aggregator():
             print(f"aggregating {output} ...")
             ds.to_netcdf(output, compute=False).compute()
             return output
+
+
+class CW5E5ClimateAggregator(Aggregator):
+    """An aggregator to compute CHELSA-W5E5 climatologies from daily means.
+
+    Call parameters
+    ---------------
+    variable : 'tasmax', 'tas', 'tasmin', 'rsds', 'pr'
+        The short name for the CHELSA-W5E5 variable aggregated among:
+        - daily mean precipitation ('pr', kg m-2 s-1),
+        - daily mean surface downwelling shortwave dadiation ('rsds', W m-2),
+        - daily mean near-surface air temperature ('tas', K),
+        - daily maximum near surface air temperature ('tasmax', K),
+        - daily minimum near surface air temperature ('tasmin', K).
+    start : int
+        The aggregation start year between 1979 and 2016.
+    end : int
+        The aggregation end year between 1979 and 2016.
+    month : int
+        The month for which data is downloaded data between 1 and 12.
+    """
+
+    def inputs(self, *args):
+        """Return paths of input files, downloading as necessary."""
+        variable, start, end, month = args
+        downloader = hyoga.open.downloader.CW5E5DailyDownloader()
+        years = range(start, end+1)
+        paths = (downloader(variable, year, month) for year in years)
+        return paths
+
+    def output(self, *args):
+        """Return path of downloaded file."""
+        variable, start, end, month = args
+        xdg_cache = os.environ.get("XDG_CACHE_HOME", os.path.join(
+            os.path.expanduser('~'), '.cache'))
+        return os.path.join(
+            xdg_cache, 'hyoga', 'cw5e5', 'clim', f'cw5e5.{variable}.mon.'
+            f'{start % 100:02d}{end % 100:02d}.avg.{month:02d}.nc')
